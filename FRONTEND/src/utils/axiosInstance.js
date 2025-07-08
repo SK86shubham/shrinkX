0a76e0aaefbbd6b244e7d9f0cd21e7aa286b7740
Shubham Singh <skshubhamsingh84@gmailcom>
2025-07-08 12:07:10 +0530

    axios

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios"
+ 
+const baseURL = import.meta.env.VITE_API_URL ;
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:8000",
+    baseURL,
     timeout:10000, //10s
     withCredentials:true
 })
